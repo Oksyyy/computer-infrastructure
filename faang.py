eed38a0061b2b56744cb9e2129054c35565369c5
Oksana Abrosimova <157732253+Oksyyy@users.noreply.github.com>
2025-11-12 23:05:42 +0000

    Added a script as part of problem 3

---
+++ faang.py
@@ -0,0 +1,23 @@
+
+
+# Date and time handling
+import datetime as dt
+
+# Data manipulation
+import pandas as pd
+
+# Yahoo Finance data
+import yfinance as yf
+
+# Download historical data for the last 5 days
+# data = yf.download('META AAPL AMZN NFLX GOOG', start = five_days_ago, end = now , interval='1h')
+df = yf.download('META AAPL AMZN NFLX GOOG', period='5d', interval='1h')
+
+# Get today's date 
+now = dt.datetime.now()
+
+# File name
+filename =  'data/' + dt.datetime.now().strftime('%Y%m%d-%H%M%S') + '.csv'
+
+# Save to CSV
+df.to_csv(filename)
